20f2b32bebcc20bdbb209aaea1038bf6ead76945
Jacob Lee <jacobtwlee@gmail.com>
2016-10-08 01:27:40 -0700

    update task 4 with new specification

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,31 @@ var cart = {}
 var products = {}
 var inactiveTime = 0;
 
-function showCart(){
-    var cartStr = "";
+// Display the cart contents to the user
+// Each item is presented in its own alert with a gap of 5 seconds between each alert
+function showCart() {
+    // we want the alerts displayed to the user to reflect the status of the cart at
+    // the time of clicking the button, so we get an array of the keys instead of operating
+    // on the cart object properties directly
+    var cartProducts = Object.keys(cart);
     
-    for (var productName in cart) {
-        cartStr += productName + ": " + cart[productName] + "\n";
+    // we don't want to allow the user to show multiple cart alerts at the same time, so
+    // whenever the user clicks the button we cancel the current sequence of alerts
+    clearTimeout(window.showCartTimeoutId);
+    
+    // recursively display all products in cart
+    function showPartForProduct(i) {
+        if (i < cartProducts.length) {
+            cartStr = "Product: " + cartProducts[i] + "\nQuantity: " + cart[cartProducts[i]];
+            alert("Cart contents:\n\n" + cartStr);
+            window.showCartTimeoutId = window.setTimeout(showPartForProduct.bind(this, i+1), 5000);
+        }
     }
     
-    cartStr = cartStr || "Cart is empty.";
-    
-    window.alert("Cart contents: \n\n" + cartStr);
-    inactiveTime = 0;
+    showPartForProduct(0); // start showing products
 }
 
+// Adds a product to the global cart and adjusts its quantity in the global products
 function addToCart(productName) {
     inactiveTime = 0; // reset the timer
   
@@ -28,12 +40,12 @@ function addToCart(productName) {
         }
         
         products[productName] -= 1;
-        // alert("Added " + productName + " to cart.");
     } else {
         alert("Couldn't add " + productName + " to cart (item is out of stock)");
     }
 }
 
+// Removes a product from the global cart and adjusts its quanity in the global products
 function removeFromCart(productName) {
     inactiveTime = 0; // reset the timer
     
@@ -43,14 +55,13 @@ function removeFromCart(productName) {
         
         if (cart[productName] == 0) {
             delete cart[productName];
-        }
-        
-        // alert("Removed " + productName + " from cart.");
+        }        
     } else {
         alert("Couldn't remove " + productName + " from cart (does not exist in cart)");
     }
 }
 
+// Checks the current inactive time and display a message to the user if they exceed the timeout
 function checkTimer() {
     var timeout = 30;
     
@@ -59,9 +70,11 @@ function checkTimer() {
         inactiveTime = 0;
     }
     
+    // increment the inactive time by 1 every second
     inactiveTime += 1;
 }
 
+// Initialize cart and product features
 (function setup() {
     var productElements = document.querySelectorAll(".product");
     var maxQuantity = 5;
