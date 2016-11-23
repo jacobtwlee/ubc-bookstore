7eaed4396c920a9f46baaac91d03e4f6706e991e
Jacob Lee <jacobtwlee@gmail.com>
2016-11-23 11:42:29 -0800

    update db initialization to use new db

--- initdb.js
+++ initdb.js
@@ -1,62 +1,77 @@
+conn = new Mongo();
+db = conn.getDB("cpen400a_group12");
+
+collections = ["products", "orders", "users"];
+
+// remove then create all our collections
+collections.forEach(
+    function(collection_name) {
+        print(collection_name);
+        db[collection_name].remove({});
+        db.createCollection(collection_name);
+    }
+);
+
+// populate the products collection
 db.products.insert({
    "KeyboardCombo":{
       "price":30,
       "quantity":1,
-      "url":"https://cpen400a.herokuapp.com/images/KeyboardCombo.png"
+      "url":"/images/products/KeyboardCombo.png"
    },
    "Mice":{
       "price":7,
       "quantity":4,
-      "url":"https://cpen400a.herokuapp.com/images/Mice.png"
+      "url":"/images/products/Mice.png"
    },
    "PC1":{
       "price":329,
       "quantity":10,
-      "url":"https://cpen400a.herokuapp.com/images/PC1.png"
+      "url":"/images/products/PC1.png"
    },
    "PC2":{
       "price":375,
       "quantity":8,
-      "url":"https://cpen400a.herokuapp.com/images/PC2.png"
+      "url":"/images/products/PC2.png"
    },
    "PC3":{
       "price":345,
       "quantity":10,
-      "url":"https://cpen400a.herokuapp.com/images/PC3.png"
+      "url":"/images/products/PC3.png"
    },
    "Tent":{
       "price":30,
       "quantity":10,
-      "url":"https://cpen400a.herokuapp.com/images/Tent.png"
+      "url":"/images/products/Tent.png"
    },
    "Box1":{
       "price":6,
       "quantity":5,
-      "url":"https://cpen400a.herokuapp.com/images/Box1.png"
+      "url":"/images/products/Box1.png"
    },
    "Box2":{
       "price":7,
       "quantity":3,
-      "url":"https://cpen400a.herokuapp.com/images/Box2.png"
+      "url":"/images/products/Box2.png"
    },
    "Clothes1":{
       "price":20,
       "quantity":10,
-      "url":"https://cpen400a.herokuapp.com/images/Clothes1.png"
+      "url":"/images/products/Clothes1.png"
    },
    "Clothes2":{
       "price":25,
       "quantity":5,
-      "url":"https://cpen400a.herokuapp.com/images/Clothes2.png"
+      "url":"/images/products/Clothes2.png"
    },
    "Jeans":{
       "price":36,
       "quantity":6,
-      "url":"https://cpen400a.herokuapp.com/images/Jeans.png"
+      "url":"/images/products/Jeans.png"
    },
    "Keyboard":{
       "price":18,
       "quantity":4,
-      "url":"https://cpen400a.herokuapp.com/images/Keyboard.png"
+      "url":"/images/products/Keyboard.png"
    }
- });
+});
